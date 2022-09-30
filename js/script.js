8209e4e3a88c9294c2798cea0343e9def7de8506
damacosta <d.mariano.costa@gmail.com>
2022-09-30 17:42:57 -0300

    Primeiro Commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+function clock() {
+
+    var h = new Date().getHours();
+    var m = new Date().getMinutes();
+    var s = new Date().getSeconds();
+
+    var hours = document.getElementById("hours").innerHTML = h;
+    var minutes = document.getElementById("minutes").innerHTML = m;
+    var seconds= document.getElementById("seconds").innerHTML = s;
+
+}
+
+var interval = setInterval(clock, 1000);
